290d004bd9edc84cb54784b959d99397a09bcbe5
Liszt21 <1832666492@qq.com>
2021-09-07 17:06:22 +0800

    add axios

---
+++ src/components/Ping.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+
+const message = ref("ping...");
+
+axios.get("/api/ping").then((response) => {
+  message.value = response.data.message;
+});
+</script>
+
+<template lang="pug">
+div.ping {{message}}
+
+</template>
