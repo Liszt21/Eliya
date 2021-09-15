0b0a2012baba73d299951f4c500c87b6737c37ed
Liszt21 <1832666492@qq.com>
2021-09-15 13:53:01 +0800

    import antd.less

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import { Antdv } from "./plugins"
 import router from './router'
 import "./themes"
+import "./styles"
 
 createApp(App).use(router).use(Antdv).mount('#app')
